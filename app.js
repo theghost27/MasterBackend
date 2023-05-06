3b0bee762e6a5d5c3cdbbf9c56023eb79fcca2b3
ABHINAV JHA <75615092+ABHINAV-JHA-27@users.noreply.github.com>
2023-05-06 23:39:58 +0530

    Added User Route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./src/Middlewares/Error.js";
+import userRouter from "./src/Routes/User.js";
 
 config({
     path: "./config.env",
@@ -13,6 +14,8 @@ export const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+
+app.use("/api/v1/users", userRouter);
 
 app.get("/", (req, res) => {
     res.send("Server is Working !!");
